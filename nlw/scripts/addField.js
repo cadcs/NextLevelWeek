cfe5e728c44394253a173f1474a5cfb676e9525f
Caio Alexandre <67668645+cadcs@users.noreply.github.com>
2020-08-07 15:07:02 -0300

    Next-Level-Week
    03/08 à 07/08

---
+++ nlw/scripts/addField.js
@@ -0,0 +1,9 @@
+
+document.querySelector("#add-time"); 
+addEventListener('click', cloneField) //MANO PQ O PONTO NAO FUNCIONA????!?!?!??!?!
+
+
+
+function cloneField() {
+    console.log("bão demais")
+}
