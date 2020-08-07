87355e8e57975f8eb10729bcf4ebe4c4452ca92d
Caio Alexandre <67668645+cadcs@users.noreply.github.com>
2020-08-07 18:04:32 -0300

    Next-Level-Week
    03/08 à 07/08

--- nlw/scripts/addField.js
+++ nlw/scripts/addField.js
@@ -1,9 +1,28 @@
 
-document.querySelector("#add-time"); 
-addEventListener('click', cloneField) //MANO PQ O PONTO NAO FUNCIONA????!?!?!??!?!
+document.querySelector("#add-time")
+    .addEventListener('click', cloneField)
 
 
 
 function cloneField() {
-    console.log("bão demais")
-}
+
+
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+
+    const fields = newFieldContainer.querySelectorAll('input')
+
+
+    
+    fields.forEach(function (field) {
+        field.value = ""
+    })
+    // fields[0].value = ""
+    // fields[1].value = ""
+
+
+
+
+
+
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+} 
